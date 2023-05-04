268202df1d749664f38a09001981fa5fbc0a6d1d
pete5315 <124735535+pete5315@users.noreply.github.com>
2023-05-04 16:52:44 -0500

    Completed part 2 and stretch goals

--- script.js
+++ script.js
@@ -9,33 +9,61 @@ function onReady() {
     $('#green-btn').on('click', appendGreenBlock);
     $('#yellow-btn').on('click', appendYellowBlock);
 
-
+let red=1, blue=1, green=1, yellow=1;
 
     function appendRedBlock() {
         $('#blocks').append('<div class="block red-fill"></p>');
+        redRecount();
+    }
+
+    function redRecount() {
+        $('#red-count').empty();
+        $('#red-count').append($('.red-fill').length);    
     }
 
     function appendBlueBlock() {
         $('#blocks').append('<div class="block blue-fill"></p>');
+        $('#blue-count').empty();
+        blue++;
+        $('#blue-count').append(blue);
+    }
+
+    function blueRecount() {
+        $('#blue-count').empty();
+        $('#blue-count').append($('.blue-fill').length);    
     }
 
     function appendYellowBlock() {
         $('#blocks').append('<div class="block yellow-fill"></p>');
+        $('#yellow-count').empty();
+        yellow++;
+        $('#yellow-count').append(yellow);
+    }
+
+    function yellowRecount() {
+        $('#yellow-count').empty();
+        $('#yellow-count').append($('.yellow-fill').length);    
     }
 
     function appendGreenBlock() {
         $('#blocks').append('<div class="block green-fill"></p>');
+        $('#green-count').empty();
+        green++;
+        $('#green-count').append(green);
+    }
+
+    function greenRecount() {
+        $('#green-count').empty();
+        $('#green-count').append($('.green-fill').length);    
     }
 
 
-
-
-
-    
-
-/* 
     function removeBlock() {
         $(this).remove();
-    } */
+        redRecount();
+        blueRecount();
+        greenRecount();
+        yellowRecount()
+    }
 
 }
