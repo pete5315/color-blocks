ba3a7e3774e776d46fce66c3da6f83d153527f27
pete5315 <124735535+pete5315@users.noreply.github.com>
2023-05-04 16:38:54 -0500

    Part 1 complete

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
+$(onReady)
+
 console.log('🟥 🟦 🟩 🟨');
+
+function onReady() {
+    $('#blocks').on('click', '.block', removeBlock);
+    $('#red-btn').on('click', appendRedBlock);
+    $('#blue-btn').on('click', appendBlueBlock);
+    $('#green-btn').on('click', appendGreenBlock);
+    $('#yellow-btn').on('click', appendYellowBlock);
+
+
+
+    function appendRedBlock() {
+        $('#blocks').append('<div class="block red-fill"></p>');
+    }
+
+    function appendBlueBlock() {
+        $('#blocks').append('<div class="block blue-fill"></p>');
+    }
+
+    function appendYellowBlock() {
+        $('#blocks').append('<div class="block yellow-fill"></p>');
+    }
+
+    function appendGreenBlock() {
+        $('#blocks').append('<div class="block green-fill"></p>');
+    }
+
+
+
+
+
+    
+
+/* 
+    function removeBlock() {
+        $(this).remove();
+    } */
+
+}
